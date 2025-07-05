e3901e1cfd3ebd8804c567c3b06666e93a30794a
Hashir <hashirahmed868@gmail.com>
2025-07-05 22:12:25 +0500

    changed type module to common js

--- GetImage.js
+++ GetImage.js
@@ -1,6 +1,6 @@
-import fetch from 'node-fetch';
-import FormData from 'form-data';
-import { v2 as cloudinary } from 'cloudinary';
+const fetch = require('node-fetch');
+const FormData = require('form-data');
+const cloudinary = require('cloudinary').v2;
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 const dotenv = require("dotenv");
 dotenv.config();
@@ -9,11 +9,11 @@ dotenv.config();
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
 async function getImage(previewText) {
-    if (!previewText || content.trim().length === 0) {
+    if (!previewText || previewText.trim().length === 0) {
         return "No preview available.";
     }
     const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
-    const result = await model.generateContent(`I want to generate a cover image based on the content below. Carefully read the article and extract the core visual theme. Respond with a single, well-crafted image generation prompt that reflects the article’s essence. Do not include any titles, summaries, explanations, or additional text — only the raw prompt suitable for an AI image generator. Here is the content: ${previewText}`);
+    const result = await model.generateContent(`I want to generate a cartoonize cover image based on the content below. Carefully read the article and extract the core visual theme. Respond with a single, well-crafted cartoonize image generation prompt that reflects the article’s essence. Do not include any titles, summaries, explanations, or additional text — only the raw prompt suitable for an AI image generator. Here is the content: ${previewText}`);
     const response = await result.response;
     const imagePrompt = response.text();
 
@@ -32,7 +32,7 @@ async function getImage(previewText) {
     const stabilityResponse = await fetch("https://api.stability.ai/v2beta/stable-image/generate/sd3", {
         method: "POST",
         headers: {
-            Authorization: "Bearer sk-t3CFNqj9LOiu9WATYVmujdJRFhUfLVWk0IVNxaNqRGXKY374",
+            Authorization: `Bearer ${process.env.STABILITY_API}`,
             Accept: "image/*",
             ...form.getHeaders(),
         },
@@ -41,8 +41,7 @@ async function getImage(previewText) {
 
 
     if (!stabilityResponse.ok) {
-        const error = await stabilityResponse.text();
-        throw new Error(`Stability API Error: ${stabilityResponse.status} - ${error}`);
+       return null;
     }
 
     const buffer = Buffer.from(await stabilityResponse.arrayBuffer());
